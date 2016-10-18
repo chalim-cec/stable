c9458e057e47c6f55dcc8b464b0541a32e2bd12a
Charlotte <charlotte.lim@customerexperience.com.au>
2016-10-19 10:13:37 +1100

    resize working

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
-/*
-if (window.matchMedia( "(max-width: 750px)")) {
-  $("#officeContainer").addClass("col-sm-10");
+$(document).ready(function() {
+  gameSize();
+});
 
+$( window ).resize(function() {
+  gameSize();
+});
+
+function gameSize() {
+  console.log("whaddup");
+  var mql = window.matchMedia("(max-width: 710px)");
+
+  if (mql.matches) {
+    console.log("yes");
+    $("#office-container").addClass("col-sm-10");
+    $("#office-container").css({"float": "none", "display": "block", "width": "auto"});
+    $("#menu-container").addClass("col-sm-10");
+    $("#menu-container").css({"float": "none", "display": "block", "width": "auto"});
+    $("#game-container").css({"display": "block"});
+  } else {
+    console.log("no");
+    $("#menu-container").removeClass("col-sm-10");
+    $("#menu-container").css({"float": "right", "width": "200"});
+    $("#office-container").removeClass("col-sm-10");
+    $("#office-container").css({"float": "right", "width": "500"}); 
+    $("#game-container").css({"display": "inline-block"}); 
+  }
 }
-*/
 
 // target elements with the "draggable" class
 interact('.draggable')
@@ -30,20 +52,20 @@ interact('.draggable')
     onend: function (event) {
       var textEl = event.target.querySelector('p');
     }
-  });
+});
 
-  function dragMoveListener (event) {
-    var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+function dragMoveListener (event) {
+  var target = event.target,
+      // keep the dragged position in the data-x/data-y attributes
+      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-    // translate the element
-    target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+  // translate the element
+  target.style.webkitTransform =
+  target.style.transform =
+    'translate(' + x + 'px, ' + y + 'px)';
 
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
+  // update the posiion attributes
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
+}
